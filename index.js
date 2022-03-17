26835c17682458e2143618d665245abedd0509a6
ro_g <97912154+Ro-Galvan@users.noreply.github.com>
2022-03-17 12:52:43 -0400

    imported the inquirer package and started writing some questions

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 // TODO: Include packages needed for this application
+// importing the package: inquirer
+const inquirer = require('inquirer');
 
+inquirer.prompt([
+    {
+        type: 'input',
+        message: 'Your Project\'s Title?',
+        name: 'title'
+    },
+
+    {
+        type: 'input',
+        message: 'Provide a short description of your project',
+        name: 'description'
+    },
+    
+    {
+        type: 'input',
+        message: '',
+        name: 'description'
+    },
+])
 // TODO: Create an array of questions for user input
 const questions = [];
 
